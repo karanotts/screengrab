e8f25aed1f6c798e263691bc51c05f95e0600416
karanotts <nottinghamkara@gmail.com>
2019-08-19 00:53:50 +0100

    corrected typo

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     THUN_AUTH = os.environ.get('THUN_AUTH')
     UPLOAD_FOLDER = os.path.join(basedir, 'screengrab/static/uploads/')
