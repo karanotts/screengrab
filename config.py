e5a0ce1ea20b58e69bdfcc6cd03c60039dce7b23
karanotts <nottinghamkara@gmail.com>
2019-08-19 01:05:00 +0100

    deploying to heroku

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     THUN_AUTH = os.environ.get('THUN_AUTH')
     UPLOAD_FOLDER = os.path.join(basedir, 'screengrab/static/uploads/')
